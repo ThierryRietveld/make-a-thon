d76355dac4c65f99cce6f57bb6fa46a8503d565c
Danny Berkelaar <dannyberkelaar@gmail.com>
2018-04-06 09:43:10 +0200

    lekker setupje

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/index.css';
+
+// pages
+import Home from './containers/home';
+import Info from './containers/info';
+import Contact from './containers/contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Home />
+        <Info />
+        <Contact />
       </div>
     );
   }
